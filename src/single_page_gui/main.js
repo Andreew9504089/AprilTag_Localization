7bd0c5cf38d96842321d1b9d7998d9facb7b765d
AndrewtheDreamer <61780723+Andreew9504089@users.noreply.github.com>
2022-08-23 11:54:08 +0800

    Add files via upload

--- src/single_page_gui/main.js
+++ src/single_page_gui/main.js
@@ -4,14 +4,16 @@ var app = new Vue({
         connected: false,
         failure: false,
         ros: null,
-        ws_address: 'ws://0.0.0.0:9090',
+        ws_address: '9090',
         logs: [],
         loading: false,
         topic: null,
         message: null,
         enable_takeoff: false,
         enable_return: false,
-        destination: '',
+        control_pub: null,
+        control_signal: null,
+        destination: null,
         direction: 1,
         cruise_height: 1,
         hover_time: 15
@@ -19,21 +21,39 @@ var app = new Vue({
     methods: {
         connect: function(){
             console.log('Connecting...')
+
             this.ros = new ROSLIB.Ros({
-                url: this.ws_address
+                url: 'ws://0.0.0.0:' + this.ws_address
             })
+
             this.ros.on('connection', () => {
                 this.logs.unshift((new Date()).toTimeString() + ' - connected')
                 this.connected = true
                 this.failure = false
                 console.log('Connected')
                 
+                // Create a control signal publisher: take off 1, return 2, stop 0
+                this.control_pub = new ROSLIB.Topic({
+                    ros: this.ros,
+                    name: '/control_phase',
+                    messageType: 'std_msgs/Int8'
+                });
+
+                this.control_signal = new ROSLIB.Message({
+                    data: 0
+                })
+
+                this.control_pub.publish(this.control_signal)
+                this.logs.unshift((new Date()).toTimeString() + 'Control Signal: ' + this.control_signal.data)
+
                 //TODO
                 /* Determine whether the uav is static*/ 
                 //if yes
                 this.enable_takeoff = true
                 this.enable_return = false
 
+                document.getElementById("cam_topic").style.display = "block";
+                document.getElementById("mjpeg").style.display = "block";
                 this.showCamera()
             })
             this.ros.on('error', (error) => {
@@ -49,53 +69,105 @@ var app = new Vue({
                 }   
             })
         },
+
         disconnect: function(){
             this.ros.close()
             this.connected = false
             console.log('Connection Closed')
+            document.getElementById("cam_topic").style.display = "block";
+            document.getElementById("mjpeg").style.display = "none";
         },
+
         showCamera: function(){
             console.log('set camera method')
+
+            const image_port = parseInt(this.ws_address, 10) - 1010
+            
             this.cameraViewer = new MJPEGCANVAS.Viewer({
                 divID: 'mjpeg',
                 host: '0.0.0.0',
                 width: 640,
                 height: 480,
                 topic: '/iris1/camera_forward/color/image_raw',
-                port: 8080,
+                port: image_port,
             })
+
             document.getElementById("cam_topic").innerHTML = '/iris1/camera_forward/color/image_raw';
         },
+
         takeoff: function(){
             console.log('UAV takeoff')
             this.logs.unshift((new Date()).toTimeString() + 'UAV takeoff')
+
             //TODO
             /* Publish Takeoff signal */
+            // Create a control signal publisher: take off 1, return 2, stop 0
+            this.control_signal = new ROSLIB.Message({
+                data: 1
+            })
+
+            this.control_pub.publish(this.control_signal)
+            this.logs.unshift((new Date()).toTimeString() + 'Control Signal: ' + this.control_signal.data)
+
             this.enable_return = true
             this.enable_takeoff = false
         },
+
         returning: function(){
             console.log('uav returning')
             this.logs.unshift((new Date()).toTimeString() + 'UAV returning')
             
             //TODO
             /* Publish uav landing signal */
+            // Create a control signal publisher: take off 1, return 2, stop 0
+            this.control_signal = new ROSLIB.Message({
+                data: 2
+            })
+
+            this.control_pub.publish(this.control_signal)
+            this.logs.unshift((new Date()).toTimeString() + 'Control Signal: ' + this.control_signal.data)
+
             this.enable_return = false
             this.enable_takeoff = true
         },
+
         stop: function(){
             console.log('uav stopping')
             this.logs.unshift((new Date()).toTimeString() + 'UAV stopping')
             //TODO
             /*Send emergency stop signal (LAND DIRECTLY)*/
+            // Create a control signal publisher: take off 1, return 2, stop 0
+            this.control_signal = new ROSLIB.Message({
+                data: 0
+            })
+
+            this.control_pub.publish(this.control_signal)
+            this.logs.unshift((new Date()).toTimeString() + 'Control Signal: ' + this.control_signal.data)
+
             this.enable_return = false
             this.enable_takeoff = false
         },
+
         startFlight: function(){
             //TODO
             /*Send goal point, direction, hover_time & cruise_height*/
+            // Create a control signal publisher: take off 1, return 2, stop 0
+            const destination = parseInt(this.destination, 10)
 
+            this.dest_pub = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/flight_destination',
+                messageType: 'std_msgs/Int8'
+            });
+
+            var flight_destination = new ROSLIB.Message({
+                data: destination
+            })
+
+            this.dest_pub.publish(flight_destination)
+            this.logs.unshift((new Date()).toTimeString() + 'Flying to tag ' + flight_destination.data)
         },
+
         chooseDirection: function(action){
             if(document.getElementById("left_btn").disabled || document.getElementById("right_btn").disabled){
                 document.getElementById("left_btn").disabled = false;
